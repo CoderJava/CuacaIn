53d7bd9821fa139ea8a37f0a607d1d6651d8b560
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-04-22 17:24:40 +0700

    Initial commit

---
+++ app/src/main/java/com/ysn/cuacain/MainActivity.kt
@@ -0,0 +1,19 @@
+/*
+ * Created by YSN Studio on 4/22/18 5:24 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 4/22/18 4:58 PM
+ */
+
+package com.ysn.cuacain
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/ysn/cuacain/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+/*
+ * Created by YSN Studio on 4/22/18 5:24 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 4/22/18 4:58 PM
+ */
+
+package com.ysn.cuacain
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
