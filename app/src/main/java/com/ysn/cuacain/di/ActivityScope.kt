8cba4338ed30bc73a2eef5169f8cec59161df4c4
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-10 23:07:43 +0700

    Create ActivityScope.kt file

---
+++ app/src/main/java/com/ysn/cuacain/di/ActivityScope.kt
@@ -0,0 +1,14 @@
+/*
+ * Created by YSN Studio on 5/10/18 11:07 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/10/18 10:47 PM
+ */
+
+package com.ysn.cuacain.di
+
+import javax.inject.Scope
+
+@Scope
+@Retention
+annotation class ActivityScope
