cb653de7cc1fc07cf9f523a4513f810c033ccc6c
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-10 23:11:30 +0700

    Create MainActivity.kt and activity_main.xml file

---
+++ app/src/main/java/com/ysn/cuacain/views/ui/main/MainActivity.kt
@@ -0,0 +1,20 @@
+/*
+ * Created by YSN Studio on 5/10/18 11:11 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/10/18 10:37 PM
+ */
+
+package com.ysn.cuacain.views.ui.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ysn.cuacain.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
