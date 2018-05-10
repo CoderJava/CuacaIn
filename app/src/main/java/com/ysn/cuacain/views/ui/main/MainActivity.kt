9bb81fa67b614d537f0d1c98f8836e342d0b0817
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-10 23:21:58 +0700

    Create MVP for MainActivity

--- app/src/main/java/com/ysn/cuacain/views/ui/main/MainActivity.kt
+++ app/src/main/java/com/ysn/cuacain/views/ui/main/MainActivity.kt
@@ -1,20 +1,40 @@
 /*
- * Created by YSN Studio on 5/10/18 11:11 PM
+ * Created by YSN Studio on 5/10/18 11:21 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/10/18 10:37 PM
+ * Last modified 5/10/18 11:20 PM
  */
 
 package com.ysn.cuacain.views.ui.main
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.ysn.cuacain.R
+import com.ysn.cuacain.di.component.activity.DaggerMainActivityComponent
+import com.ysn.cuacain.di.module.activity.main.MainActivityModule
+import com.ysn.cuacain.views.base.BaseActivity
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), MainView {
+
+    @Inject
+    lateinit var presenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onError() {
+        // TODO: do something in here if needed
+    }
+
+    override fun onActivityInject() {
+        DaggerMainActivityComponent.builder()
+                .appComponent(getAppComponent())
+                .mainActivityModule(MainActivityModule())
+                .build()
+                .inject(this)
+        presenter.attachView(this)
+    }
+
 }
---
+++ app/src/main/java/com/ysn/cuacain/views/ui/main/MainPresenter.kt
@@ -0,0 +1,15 @@
+/*
+ * Created by YSN Studio on 5/10/18 11:21 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/10/18 11:16 PM
+ */
+
+package com.ysn.cuacain.views.ui.main
+
+import com.ysn.cuacain.api.Endpoints
+import com.ysn.cuacain.views.base.mvp.BasePresenter
+
+class MainPresenter constructor(private val endpoints: Endpoints): BasePresenter<MainView>() {
+
+}
