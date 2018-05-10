a2fde1869e6b07308f83abf57d0fb6f3e51b900a
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-10 23:08:21 +0700

    Create Endpoints.kt file

---
+++ app/src/main/java/com/ysn/cuacain/api/Endpoints.kt
@@ -0,0 +1,11 @@
+/*
+ * Created by YSN Studio on 5/10/18 11:08 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/10/18 10:37 PM
+ */
+
+package com.ysn.cuacain.api
+
+interface Endpoints {
+}
