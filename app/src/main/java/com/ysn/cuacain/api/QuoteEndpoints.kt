b121572b9ed08ca47b1c085285be6ff4ae84dd9c
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-11 22:41:34 +0700

    Renamed Endpoints to QuoteEndpoints file

---
+++ app/src/main/java/com/ysn/cuacain/api/QuoteEndpoints.kt
@@ -0,0 +1,19 @@
+/*
+ * Created by YSN Studio on 5/11/18 10:41 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/11/18 10:39 PM
+ */
+
+package com.ysn.cuacain.api
+
+import com.ysn.cuacain.model.quote.ResponseQuoteOfToday
+import io.reactivex.Observable
+import retrofit2.http.GET
+
+interface QuoteEndpoints {
+
+    @GET("qod")
+    fun getQuoteOfToday(): Observable<ResponseQuoteOfToday>
+
+}
