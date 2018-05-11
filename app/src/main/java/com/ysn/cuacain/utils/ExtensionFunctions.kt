38da153e58f67be614f0e37ffa3e0f3eeb9b942e
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-11 22:56:13 +0700

    Create ExtensionFunctions.kt file

---
+++ app/src/main/java/com/ysn/cuacain/utils/ExtensionFunctions.kt
@@ -0,0 +1,22 @@
+/*
+ * Created by YSN Studio on 5/11/18 10:56 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/11/18 9:22 PM
+ */
+
+package com.ysn.cuacain.utils
+
+import android.view.View
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
