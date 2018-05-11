842ed993b054f69ce903903fb3106ef077f9e4b9
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-11 23:00:19 +0700

    Add feature load data quote of today in MainActivity

--- app/src/main/java/com/ysn/cuacain/views/ui/main/MainActivity.kt
+++ app/src/main/java/com/ysn/cuacain/views/ui/main/MainActivity.kt
@@ -1,8 +1,8 @@
 /*
- * Created by YSN Studio on 5/10/18 11:21 PM
+ * Created by YSN Studio on 5/11/18 11:00 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/10/18 11:20 PM
+ * Last modified 5/11/18 9:25 PM
  */
 
 package com.ysn.cuacain.views.ui.main
@@ -11,7 +11,11 @@ import android.os.Bundle
 import com.ysn.cuacain.R
 import com.ysn.cuacain.di.component.activity.DaggerMainActivityComponent
 import com.ysn.cuacain.di.module.activity.main.MainActivityModule
+import com.ysn.cuacain.utils.invisible
+import com.ysn.cuacain.utils.visible
 import com.ysn.cuacain.views.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 import javax.inject.Inject
 
 class MainActivity : BaseActivity(), MainView {
@@ -22,6 +26,7 @@ class MainActivity : BaseActivity(), MainView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        doLoadQuoteOfToday()
     }
 
     override fun onError() {
@@ -35,6 +40,34 @@ class MainActivity : BaseActivity(), MainView {
                 .build()
                 .inject(this)
         presenter.attachView(this)
+    }
+
+    private fun doLoadQuoteOfToday() {
+        showLoadingQuoteOfToday()
+        presenter.onLoadQuoteOfToday()
+    }
+
+    override fun loadQuoteOfToday(quote: String, author: String) {
+        hideLoadingQuoteOfToday()
+        text_view_value_quote_of_today_activity_main.text = quote
+        text_view_author_quote_of_today_activity_main.text = author
+    }
+
+    override fun loadQuoteofTodayFailed(message: String?) {
+        hideLoadingQuoteOfToday()
+        toast(message!!)
+    }
+
+    private fun showLoadingQuoteOfToday() {
+        text_view_value_quote_of_today_activity_main.invisible()
+        text_view_author_quote_of_today_activity_main.invisible()
+        progress_bar_quote_of_today_activity_main.visible()
+    }
+
+    private fun hideLoadingQuoteOfToday() {
+        text_view_value_quote_of_today_activity_main.visible()
+        text_view_author_quote_of_today_activity_main.visible()
+        progress_bar_quote_of_today_activity_main.invisible()
     }
 
 }
--- app/src/main/java/com/ysn/cuacain/views/ui/main/MainView.kt
+++ app/src/main/java/com/ysn/cuacain/views/ui/main/MainView.kt
@@ -1,8 +1,8 @@
 /*
- * Created by YSN Studio on 5/10/18 11:21 PM
+ * Created by YSN Studio on 5/11/18 11:00 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/10/18 11:16 PM
+ * Last modified 5/11/18 9:25 PM
  */
 
 package com.ysn.cuacain.views.ui.main
@@ -10,5 +10,9 @@ package com.ysn.cuacain.views.ui.main
 import com.ysn.cuacain.views.base.mvp.BaseView
 
 interface MainView : BaseView {
+
+    fun loadQuoteOfToday(quote: String, author: String)
+
+    fun loadQuoteofTodayFailed(message: String?)
 
 }
