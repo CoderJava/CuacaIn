02e7da6bb1a15ea2ce9de87c9d5d0812b2c0f387
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-11 22:43:22 +0700

    Add annotation Named to make different retrofit instance in RetrofitModule.kt file

--- app/src/main/java/com/ysn/cuacain/di/module/RetrofitModule.kt
+++ app/src/main/java/com/ysn/cuacain/di/module/RetrofitModule.kt
@@ -1,26 +1,30 @@
 /*
- * Created by YSN Studio on 5/11/18 9:10 PM
+ * Created by YSN Studio on 5/11/18 10:43 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/11/18 9:10 PM
+ * Last modified 5/11/18 10:36 PM
  */
 
 package com.ysn.cuacain.di.module
 
+import com.ysn.cuacain.BuildConfig
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
 class RetrofitModule {
 
     @Provides
+    @Named("quote")
     @Singleton
     fun provideRetrofitClient(client: OkHttpClient): Retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL_QUOTE_ENDPOINT)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(client)
