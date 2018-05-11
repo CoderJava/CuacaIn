0824b0da620626befec1594efe87243fb7e01407
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-11 22:54:00 +0700

    Create WeatherEndpoints.kt file

---
+++ app/src/main/java/com/ysn/cuacain/api/weather/WeatherEndpoints.kt
@@ -0,0 +1,11 @@
+/*
+ * Created by YSN Studio on 5/11/18 10:54 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/11/18 10:52 PM
+ */
+
+package com.ysn.cuacain.api.weather
+
+interface WeatherEndpoints {
+}
