89988799a48be0a4a907a1bc72d11d3169d02a89
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-11 22:54:15 +0700

    Update copyright

--- app/src/main/java/com/ysn/cuacain/api/weather/WeatherEndpoints.kt
+++ app/src/main/java/com/ysn/cuacain/api/weather/WeatherEndpoints.kt
@@ -2,7 +2,7 @@
  * Created by YSN Studio on 5/11/18 10:54 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/11/18 10:52 PM
+ * Last modified 5/11/18 10:54 PM
  */
 
 package com.ysn.cuacain.api.weather
