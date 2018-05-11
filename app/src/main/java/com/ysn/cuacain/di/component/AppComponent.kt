968506cb9283fefea9c711189713b0ce91f39e4b
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-11 22:44:11 +0700

    Add annotation Named("quote") for retrofit quote in AppComponent.kt file

--- app/src/main/java/com/ysn/cuacain/di/component/AppComponent.kt
+++ app/src/main/java/com/ysn/cuacain/di/component/AppComponent.kt
@@ -1,8 +1,8 @@
 /*
- * Created by YSN Studio on 5/10/18 11:10 PM
+ * Created by YSN Studio on 5/11/18 10:44 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/10/18 11:03 PM
+ * Last modified 5/11/18 10:39 PM
  */
 
 package com.ysn.cuacain.di.component
@@ -10,26 +10,28 @@ package com.ysn.cuacain.di.component
 import android.app.Application
 import android.content.SharedPreferences
 import com.google.gson.Gson
-import com.ysn.cuacain.api.Endpoints
+import com.ysn.cuacain.api.QuoteEndpoints
 import com.ysn.cuacain.di.module.*
 import dagger.Component
 import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [(AppModule::class), (RetrofitModule::class), (ApiModule::class), (OkHttpModule::class), (SharedPreferencesModule::class)])
+@Component(modules = [(AppModule::class), (RetrofitModule::class), (QuoteApiModule::class), (OkHttpModule::class), (SharedPreferencesModule::class)])
 interface AppComponent {
 
     fun application(): Application
 
     fun gson(): Gson
 
+    @Named("quote")
     fun retrofit(): Retrofit
 
-    fun endpoints(): Endpoints
+    fun quoteEndpoints(): QuoteEndpoints
 
     fun cache(): Cache
 
