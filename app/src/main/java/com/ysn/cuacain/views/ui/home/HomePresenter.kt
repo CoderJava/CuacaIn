2fe757c1cebf15bdf68c0b84b6e20c00642c5a7b
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-12 07:40:53 +0700

    Renamed MainPresenter.kt to HomePresenter.kt file

--- app/src/main/java/com/ysn/cuacain/views/ui/home/HomePresenter.kt
+++ app/src/main/java/com/ysn/cuacain/views/ui/home/HomePresenter.kt
@@ -1,18 +1,18 @@
 /*
- * Created by YSN Studio on 5/11/18 10:55 PM
+ * Created by YSN Studio on 5/12/18 7:40 AM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/11/18 10:52 PM
+ * Last modified 5/12/18 7:38 AM
  */
 
-package com.ysn.cuacain.views.ui.main
+package com.ysn.cuacain.views.ui.home
 
 import com.ysn.cuacain.api.quote.QuoteEndpoints
 import com.ysn.cuacain.views.base.mvp.BasePresenter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
-class MainPresenter(private val quoteEndpoints: QuoteEndpoints) : BasePresenter<MainView>() {
+class HomePresenter(private val quoteEndpoints: QuoteEndpoints) : BasePresenter<HomeView>() {
 
     fun onLoadQuoteOfToday() {
         quoteEndpoints.getQuoteOfToday()
