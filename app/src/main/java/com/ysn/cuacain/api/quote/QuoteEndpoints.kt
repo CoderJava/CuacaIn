884133d17e52684a3fd17b139a486ffb37ada1e5
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-20 14:00:01 +0700

    Update import in QuoteEndpoints.kt file

--- app/src/main/java/com/ysn/cuacain/api/quote/QuoteEndpoints.kt
+++ app/src/main/java/com/ysn/cuacain/api/quote/QuoteEndpoints.kt
@@ -1,13 +1,13 @@
 /*
- * Created by YSN Studio on 5/11/18 10:53 PM
+ * Created by YSN Studio on 5/20/18 2:00 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/11/18 10:53 PM
+ * Last modified 5/20/18 1:50 PM
  */
 
 package com.ysn.cuacain.api.quote
 
-import com.ysn.cuacain.model.quote.ResponseQuoteOfToday
+import com.ysn.cuacain.model.quote.quoteoftoday.ResponseQuoteOfToday
 import io.reactivex.Observable
 import retrofit2.http.GET
 
