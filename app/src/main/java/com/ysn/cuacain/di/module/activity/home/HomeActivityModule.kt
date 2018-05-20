aa6c7d62e3d6610514f564f1c1bdbfa65b378d15
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-20 23:50:17 +0700

    Add parameter WeatherEndpoints, SharedPreferencesManager, and DbManager in HomeActivityModule.kt file

--- app/src/main/java/com/ysn/cuacain/di/module/activity/home/HomeActivityModule.kt
+++ app/src/main/java/com/ysn/cuacain/di/module/activity/home/HomeActivityModule.kt
@@ -1,14 +1,17 @@
 /*
- * Created by YSN Studio on 5/20/18 10:25 AM
+ * Created by YSN Studio on 5/20/18 11:50 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/20/18 12:00 AM
+ * Last modified 5/20/18 10:46 PM
  */
 
 package com.ysn.cuacain.di.module.activity.home
 
 import com.ysn.cuacain.api.quote.QuoteEndpoints
+import com.ysn.cuacain.api.weather.WeatherEndpoints
+import com.ysn.cuacain.db.DbManager
 import com.ysn.cuacain.di.ActivityScope
+import com.ysn.cuacain.model.sharedpreferences.SharedPreferencesManager
 import com.ysn.cuacain.views.ui.activity.home.HomePresenter
 import dagger.Module
 import dagger.Provides
@@ -18,7 +21,14 @@ class HomeActivityModule {
 
     @Provides
     @ActivityScope
-    internal fun provideHomePresenter(quoteEndpoints: QuoteEndpoints):
-            HomePresenter = HomePresenter(quoteEndpoints = quoteEndpoints)
+    internal fun provideHomePresenter(quoteEndpoints: QuoteEndpoints,
+                                      weatherEndpoints: WeatherEndpoints,
+                                      sharedPreferencesManager: SharedPreferencesManager,
+                                      dbManager: DbManager): HomePresenter = HomePresenter(
+            quoteEndpoints = quoteEndpoints,
+            weatherEndpoints = weatherEndpoints,
+            sharedPreferencesManager = sharedPreferencesManager,
+            dbManager = dbManager
+    )
 
 }
