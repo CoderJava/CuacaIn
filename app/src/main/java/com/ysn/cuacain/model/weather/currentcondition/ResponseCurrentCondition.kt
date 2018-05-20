cfc6052978c8251a254fed27a8619460d69ab285
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-20 23:45:00 +0700

    Create model ResponseCurrentCondition.kt file

---
+++ app/src/main/java/com/ysn/cuacain/model/weather/currentcondition/ResponseCurrentCondition.kt
@@ -0,0 +1,38 @@
+/*
+ * Created by YSN Studio on 5/20/18 11:45 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/20/18 11:38 PM
+ */
+
+package com.ysn.cuacain.model.weather.currentcondition
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseCurrentCondition(
+        @SerializedName("LocalObservationDateTime") val localObservationDateTime: String?,
+        @SerializedName("EpochTime") val epochTime: Int?,
+        @SerializedName("WeatherText") val weatherText: String?,
+        @SerializedName("WeatherIcon") val weatherIcon: Int?,
+        @SerializedName("IsDayTime") val isDayTime: Boolean?,
+        @SerializedName("Temperature") val temperature: Temperature,
+        @SerializedName("MobileLink") val mobileLink: String?,
+        @SerializedName("Link") val link: String?
+)
+
+data class Temperature(
+        @SerializedName("Metric") val metric: Metric,
+        @SerializedName("Imperial") val imperial: Imperial
+)
+
+data class Metric(
+        @SerializedName("Value") val value: Int?,
+        @SerializedName("Unit") val unit: String?,
+        @SerializedName("UnitType") val unitType: Int?
+)
+
+data class Imperial(
+        @SerializedName("Value") val value: Int?,
+        @SerializedName("Unit") val unit: String?,
+        @SerializedName("UnitType") val unitType: Int?
+)
