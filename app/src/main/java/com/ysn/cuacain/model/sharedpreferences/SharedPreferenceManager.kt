efc3fd92c0b22e28e6470cfc7e139f49f9e3339a
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-20 11:28:05 +0700

    Create model SharedPreferencesManaget.kt file

---
+++ app/src/main/java/com/ysn/cuacain/model/sharedpreferences/SharedPreferenceManager.kt
@@ -0,0 +1,65 @@
+/*
+ * Created by YSN Studio on 5/20/18 11:28 AM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/20/18 11:27 AM
+ */
+
+package com.ysn.cuacain.model.sharedpreferences
+
+import android.content.SharedPreferences
+import javax.inject.Inject
+
+class SharedPreferenceManager @Inject constructor(private val sharedPreferences: SharedPreferences) {
+
+    fun putDataInt(key: String, value: Int) {
+        sharedPreferences.edit()
+                .putInt(key, value)
+                .apply()
+    }
+
+    fun getDataInt(key: String) {
+        sharedPreferences.getInt(key, 0)
+    }
+
+    fun putDataBoolean(key: String, value: Boolean) {
+        sharedPreferences.edit()
+                .putBoolean(key, value)
+                .apply()
+    }
+
+    fun getDataBoolean(key: String) {
+        sharedPreferences.getBoolean(key, false)
+    }
+
+    fun putDataFloat(key: String, value: Float) {
+        sharedPreferences.edit()
+                .putFloat(key, value)
+                .apply()
+    }
+
+    fun getDataFloat(key: String) {
+        sharedPreferences.getFloat(key, 0F)
+    }
+
+    fun putDataLong(key: String, value: Long) {
+        sharedPreferences.edit()
+                .putLong(key, value)
+                .apply()
+    }
+
+    fun getDataLong(key: String) {
+        sharedPreferences.getLong(key, 0L)
+    }
+
+    fun putDataString(key: String, value: String) {
+        sharedPreferences.edit()
+                .putString(key, value)
+                .apply()
+    }
+
+    fun getDataString(key: String) {
+        sharedPreferences.getString(key, "")
+    }
+
+}
