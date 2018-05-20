1b908227e64d49bf482bdc2695e88eb4f9edf1e4
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-20 23:47:38 +0700

    Initialize ObjectBox in App.kt file

--- app/src/main/java/com/ysn/cuacain/views/base/App.kt
+++ app/src/main/java/com/ysn/cuacain/views/base/App.kt
@@ -1,16 +1,18 @@
 /*
- * Created by YSN Studio on 5/10/18 11:10 PM
+ * Created by YSN Studio on 5/20/18 11:47 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/10/18 11:04 PM
+ * Last modified 5/20/18 10:28 PM
  */
 
 package com.ysn.cuacain.views.base
 
 import android.app.Application
+import com.ysn.cuacain.db.entity.daily1dayforecast.MyObjectBox
 import com.ysn.cuacain.di.component.AppComponent
 import com.ysn.cuacain.di.component.DaggerAppComponent
 import com.ysn.cuacain.di.module.AppModule
+import io.objectbox.BoxStore
 
 class App : Application() {
 
@@ -19,14 +21,24 @@ class App : Application() {
         lateinit var appComponent: AppComponent
     }
 
+    lateinit var boxStore: BoxStore
+        private set
+
     override fun onCreate() {
         super.onCreate()
         initDagger()
+        initObjectBox()
     }
 
     private fun initDagger() {
         appComponent = DaggerAppComponent.builder()
                 .appModule(AppModule(this))
+                .build()
+    }
+
+    private fun initObjectBox() {
+        boxStore = MyObjectBox.builder()
+                .androidContext(this)
                 .build()
     }
 
