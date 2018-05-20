4f12f36dede574b6094230237c2a35ba126fc698
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-20 14:00:39 +0700

    Add endpoint getLocationWeather, getDaily1DayForecast, and getDaily12HourForecast in WeatherEndpoints.kt file

--- app/src/main/java/com/ysn/cuacain/api/weather/WeatherEndpoints.kt
+++ app/src/main/java/com/ysn/cuacain/api/weather/WeatherEndpoints.kt
@@ -1,11 +1,35 @@
 /*
- * Created by YSN Studio on 5/11/18 10:54 PM
+ * Created by YSN Studio on 5/20/18 2:00 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/11/18 10:54 PM
+ * Last modified 5/20/18 1:57 PM
  */
 
 package com.ysn.cuacain.api.weather
 
+import com.ysn.cuacain.BuildConfig
+import com.ysn.cuacain.model.weather.daily12hourforecast.ResponseDaily12HourForecast
+import com.ysn.cuacain.model.weather.daily1dayforecast.ResponseDaily1DayForecast
+import com.ysn.cuacain.model.weather.locationweather.ResponseLocationWeather
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface WeatherEndpoints {
+
+    @GET("locations/v1/cities/geoposition/search")
+    fun getLocationWeather(@Query("apikey") apiKey: String = BuildConfig.API_KEY_ACCU_WEATHER,
+                           @Query("q") q: String = ""): Observable<ResponseLocationWeather>
+
+    @GET("forecasts/v1/daily/1day/{locationId}")
+    fun getDaily1DayForecast(@Path("locationId") locationId: String = "",
+                             @Query("apikey") apiKey: String = BuildConfig.API_KEY_ACCU_WEATHER)
+            : Observable<ResponseDaily1DayForecast>
+
+    @GET("forecasts/v1/hourly/12hour/{locationId}")
+    fun getDaily12HourForecast(@Path("locationId") locationId: String = "",
+                               @Query("apikey") apiKey: String = BuildConfig.API_KEY_ACCU_WEATHER)
+            : Observable<ResponseDaily12HourForecast>
+
 }
