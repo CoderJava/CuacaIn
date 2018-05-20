4d3a20ff58f19e384731858f7487be06ca173e23
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-20 23:48:32 +0700

    Add endpoint getCurrentConditionWeather in WeatherEndpoints.kt file

--- app/src/main/java/com/ysn/cuacain/api/weather/WeatherEndpoints.kt
+++ app/src/main/java/com/ysn/cuacain/api/weather/WeatherEndpoints.kt
@@ -1,13 +1,14 @@
 /*
- * Created by YSN Studio on 5/20/18 2:00 PM
+ * Created by YSN Studio on 5/20/18 11:48 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 5/20/18 1:57 PM
+ * Last modified 5/20/18 11:38 PM
  */
 
 package com.ysn.cuacain.api.weather
 
 import com.ysn.cuacain.BuildConfig
+import com.ysn.cuacain.model.weather.currentcondition.ResponseCurrentCondition
 import com.ysn.cuacain.model.weather.daily12hourforecast.ResponseDaily12HourForecast
 import com.ysn.cuacain.model.weather.daily1dayforecast.ResponseDaily1DayForecast
 import com.ysn.cuacain.model.weather.locationweather.ResponseLocationWeather
@@ -21,6 +22,11 @@ interface WeatherEndpoints {
     @GET("locations/v1/cities/geoposition/search")
     fun getLocationWeather(@Query("apikey") apiKey: String = BuildConfig.API_KEY_ACCU_WEATHER,
                            @Query("q") q: String = ""): Observable<ResponseLocationWeather>
+
+    @GET("currentconditions/v1/{locationId}")
+    fun getCurrentConditionWeather(@Path("locationId") locationId: String = "",
+                                   @Query("apikey") apiKey: String = BuildConfig.API_KEY_ACCU_WEATHER)
+            : Observable<List<ResponseCurrentCondition>>
 
     @GET("forecasts/v1/daily/1day/{locationId}")
     fun getDaily1DayForecast(@Path("locationId") locationId: String = "",
