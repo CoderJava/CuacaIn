6e657141cfe4c4f54ecb70cca0ac25b92c55b66d
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-05-23 07:52:32 +0700

    Create UI AboutAppActivity

---
+++ app/src/main/java/com/ysn/cuacain/views/ui/activity/aboutapp/AboutAppActivity.kt
@@ -0,0 +1,20 @@
+/*
+ * Created by YSN Studio on 5/23/18 7:52 AM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 5/23/18 7:52 AM
+ */
+
+package com.ysn.cuacain.views.ui.activity.aboutapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ysn.cuacain.R
+
+class AboutAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_app)
+    }
+}
